c61ba5e488bbaf4f2293e33e168a6fba20da07e2
Hamed Saeedi <hameds@gmail.com>
2019-09-02 18:25:32 +0430

    fix #24 clean API urls
    with lowercase simple method names

--- RostamBot.Web/Controllers/AuthController.cs
+++ RostamBot.Web/Controllers/AuthController.cs
@@ -6,12 +6,14 @@ using System.Threading.Tasks;
 
 namespace RostamBot.Web.Controllers
 {
+    [Route("api/v1/auth/[action]")]
     public class AuthController : BaseApiController
     {
 
         [AllowAnonymous]
         [HttpPost]
         [Produces("application/json")]
+        [ActionName("login")]
         public async Task<IActionResult> Login([FromBody]LoginUser model)
         {
             return Ok(await Mediator.Send(model));
